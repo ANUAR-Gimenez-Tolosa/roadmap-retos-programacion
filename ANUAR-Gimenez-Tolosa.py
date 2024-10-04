a8a83425fcd6a550833ce5e072b7a0ca02791246
ANUAR-Gimenez-Tolosa <a_nu_ar@hotmail.com>
2024-10-04 20:40:24 -0300

    terminando el primer ejercicio_4_octubre

---
+++ ANUAR-Gimenez-Tolosa.py
@@ -0,0 +1,46 @@
+"""
+He aquí la URL del lenguaje que estoy utilizando
+
+https://www.python.org/
+
+"""
+
+# Comentario en una línea
+
+"""
+Así es
+para comentar
+en varias líneas -con comilas dobles-
+"""
+'''
+También se puede comentar
+en varias líneas
+-con comillas simples-
+'''
+
+#Así se crea una VARIABLE
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+#En Python no hay Constantes. Todos son Variables
+# Por convención, para representar una constante se la escribe en mayúscula -aunque sigue siendo una variable que puede ser modificada-
+MY_CONSTANT = "Mi constante"
+MY_CONSTANT = "ABCDFG"
+
+my_int = 33
+my_float = 33.5
+my_bool = True
+my_bool = False
+my_string = "Volviendo a empezar, desde cero"
+my_other_string = "siempre con la mejor onda"
+
+print("¡Hola NOSpedia!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+print(type(my_other_string))
+
+
+
